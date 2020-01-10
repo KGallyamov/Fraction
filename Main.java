a3fe9b0715fdfcaf783132ff1d36cf8b333b7808
AmirHai <amirka1410@mail.ru>
2020-01-10 16:04:27 +0400

    сложение

--- Main.java
+++ Main.java
@@ -2,25 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int[] multiply(int c1, int z1, int c2, int z2){
+    public static int[] multiply(int c1, int z1, int c2, int z2) {
         c1 *= c2;
         z1 *= z2;
         return new int[]{c1, z1};
+
     }
+
+    public static int[] plus(int c1, int z1, int c2, int z2) {
+        c1 = c1 * z2;
+        c2 = c2 * z1;
+        z1 *= z2;
+        c1 += c2;
+        for (int i = 1; i < c1; i++) {
+            if (c1 % i == 0 && z1 % i == 0) {
+                c1 /= i;
+                z1 /= i;
+            }
+        }
+        return new int[]{c1, z1}
+    }
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int chisl1 = sc.nextInt();
         int znam1 = sc.nextInt();
         int chisl2 = sc.nextInt();
         int znam2 = sc.nextInt();
         int command = sc.nextInt();
-        if(command == 1){
-            
-        }else if(command == 2){
+        if (command == 1) {
 
-        }else if(command == 3){
+        } else if (command == 2) {
 
-        }else{
+        } else if (command == 3) {
+
+        } else {
             System.out.println("Нет такой команды");
         }
     }
