fc6aeea93a7546a62ad962a89177930dc05c5747
lolept <lolept230@gmail.com>
2020-01-10 15:41:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -57,7 +57,10 @@ public class Main {
             print(multiply(chisl1, znam1, chisl2, znam2));
         }else if(command == 3){ // деление
 
-        }else{
+        }else if(command == 4) {// вычитание
+            print(minus(chisl1, znam1, chisl2, znam2));
+        }
+        else{
             System.out.println("Нет такой команды");
         }
     }
