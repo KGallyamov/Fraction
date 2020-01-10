03ef9404bc77b6cdefe39c518620bb636338abeb
AmirHai <amirka1410@mail.ru>
2020-01-10 16:51:36 +0400

    степень

--- Main.java
+++ Main.java
@@ -43,21 +43,38 @@ public class Main {
         c1 -= c2;
         return new int[]{c1, z1};
     }
-    
+    public static int[] stepen(int c1, int z1, int s1){
+        int t = c1;
+        for (int i = 1; i < s1; i++) {
+            c1 *= t;
+        }
+        t = z1;
+        for (int i = 1; i < s1; i++) {
+            s1 *= t;
+        }
+        return int[]{c1, z1};
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int command = sc.nextInt();
         int chisl1 = sc.nextInt();
         int znam1 = sc.nextInt();
-        int chisl2 = sc.nextInt();
-        int znam2 = sc.nextInt();
-        int command = sc.nextInt();
         if(command == 1){ // сложение
+            int chisl2 = sc.nextInt();
+            int znam2 = sc.nextInt();
             print(plus(chisl1, znam1, chisl2, znam2));
         }else if(command == 2){ // умножение
+            int chisl2 = sc.nextInt();
+            int znam2 = sc.nextInt();
             print(multiply(chisl1, znam1, chisl2, znam2));
         }else if(command == 3){ // деление
+            int chisl2 = sc.nextInt();
+            int znam2 = sc.nextInt();
 
-        }else{
+        }else if(command == 5){
+            int step = sc.nextInt();
+            print(stepen(chisl1, znam1 , step));
+        } else{
             System.out.println("Нет такой команды");
         }
     }
